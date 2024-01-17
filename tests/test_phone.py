be06c9df7be74b184b440730eee245837de74496
koipish <koipish@gmail.com>
2024-01-17 22:30:05 +0300

    added tests

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,5 +1,5 @@
 from src.phone import Phone
-from config import OPERATIONS_PATH
+
 
 def test_str():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
@@ -8,3 +8,11 @@ def test_str():
 def test_repr():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
+def test_number_of_sim():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    phone2 = Phone("iPhone 15", 160_000, 7, 1)
+    assert phone1.number_of_sim == 2
+    assert phone2.number_of_sim == 1
+
+
