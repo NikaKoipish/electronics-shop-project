fa976ab987538e7c6304c413117a15a1bc94308d
koipish <koipish@gmail.com>
2024-01-23 22:56:53 +0300

    added operations_path

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,11 @@
 from src.item import Item
+from config import OPERATIONS_PATH
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(OPERATIONS_PATH)
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(OPERATIONS_PATH)
     # InstantiateCSVError: Файл item.csv поврежден
